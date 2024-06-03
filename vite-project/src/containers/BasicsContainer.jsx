076ed87767986a6840686ecb575a6b44173e9a78
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-06-03 12:19:05 +0100

    UPDATED: BasicsContainer, NameContainer updated with css but still static

--- vite-project/src/containers/BasicsContainer.jsx
+++ vite-project/src/containers/BasicsContainer.jsx
@@ -4,19 +4,58 @@ const BasicsContainer = () => {
   return (
     <div className="basicsContainer">
       <div className="row">
-        <h3>Level & class</h3>
-        <h3>Background</h3>
-        <h3>Alignment</h3>
+        <div className="attributeBox">
+          <div className="levelClassContainer">
+            <div className="level">1</div>
+            <div className="class">ROGUE</div>
+          </div>
+          <hr />
+          <h3>Level & Class</h3>
+        </div>
+        <div className="attributeBox">
+          <div className="numberWord">Outlander</div>
+          <hr />
+          <h3>Background</h3>
+        </div>
+        <div className="attributeBox">
+          <div className="numberWord">Neutral</div>
+          <hr />
+          <h3>Alignment</h3>
+        </div>
       </div>
       <div className="row">
-        <h3>Race</h3>
-        <h3>Armour Class</h3>
-        <h3>Initiative</h3>
+        <div className="attributeBox">
+          <div className="numberWord">Elf</div>
+          <hr />
+          <h3>Race</h3>
+        </div>
+        <div className="attributeBox">
+          <div className="numberWord">13</div>
+          <hr />
+          <h3>Armour Class</h3>
+        </div>
+        <div className="attributeBox">
+          <div className="numberWord">+2</div>
+          <hr />
+          <h3>Initiative</h3>
+        </div>
       </div>
       <div className="row">
-        <h3>Speed</h3>
-        <h3>Hit Points</h3>
-        <h3>Proficiency</h3>
+        <div className="attributeBox">
+          <div className="numberWord">30ft</div>
+          <hr />
+          <h3>Speed</h3>
+        </div>
+        <div className="attributeBox">
+          <div className="numberWord">10</div>
+          <hr />
+          <h3>Hit Points</h3>
+        </div>
+        <div className="attributeBox">
+          <div className="numberWord">+2</div>
+          <hr />
+          <h3>Proficiency</h3>
+        </div>
       </div>
     </div>
   );
--- vite-project/src/containers/NameContainer.jsx
+++ vite-project/src/containers/NameContainer.jsx
@@ -1,13 +1,40 @@
 const NameContainer = () => {
   return (
     <div className="nameContainer">
-      <h2>Name</h2>
-      <h3>Strength</h3>
-      <h3>Dexterity</h3>
-      <h3>Constitution</h3>
-      <h3>Intelligence</h3>
-      <h3>Wisdom</h3>
-      <h3>Charisma</h3>
+      <div className="attribute">
+        <h2>Name</h2>
+        <div className="nameBox">Gehanna Dragonum</div>
+      </div>
+      <div className="attribute">
+        <h3>Strength</h3>
+        <div className="numberBox">+2</div>
+        <div className="circle">14</div>
+      </div>
+      <div className="attribute">
+        <h3>Dexterity</h3>
+        <div className="numberBox">+2</div>
+        <div className="circle">15</div>
+      </div>
+      <div className="attribute">
+        <h3>Constitution</h3>
+        <div className="numberBox">+1</div>
+        <div className="circle">13</div>
+      </div>
+      <div className="attribute">
+        <h3>Intelligence</h3>
+        <div className="numberBox">+1</div>
+        <div className="circle">12</div>
+      </div>
+      <div className="attribute">
+        <h3>Wisdom</h3>
+        <div className="numberBox">+0</div>
+        <div className="circle">10</div>
+      </div>
+      <div className="attribute">
+        <h3>Charisma</h3>
+        <div className="numberBox">+0</div>
+        <div className="circle">11</div>
+      </div>
     </div>
   );
 };
