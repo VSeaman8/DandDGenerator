d62be9ee00c73c519b721d25466809abce35bddc
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-06-18 17:14:20 +0100

    UPDATED: Split the CSS on favouriteCharPage into 7 files for ease of use

--- vite-project/src/pages/FavouriteCharPage.jsx
+++ vite-project/src/pages/FavouriteCharPage.jsx
@@ -6,6 +6,12 @@ import SkillsContainer from "../containers/SkillsContainer.jsx";
 import TraitsContainer from "../containers/TraitsContainer.jsx";
 
 import "./FavouriteCharPage.css";
+import "../containers/BasicsContainer.css";
+import "../containers/EquipmentContainer.css";
+import "../containers/FeaturesContainer.css";
+import "../containers/NameContainer.css";
+import "../containers/SkillsContainer.css";
+import "../containers/TraitsContainer.css";
 
 const FavouriteCharPage = () => {
   return (
