05e3756783464570fa7f2d217ce7f6f73c3bd81c
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-08-19 16:13:33 +0100

    UPDATED as API is GraphQL removed current server and cleared dependencies to make code cleaner

---
+++ backend/data.js
@@ -0,0 +1,9 @@
+// Mock data
+const features = {
+  personality: ["Brave", "Cautious", "Impulsive", "Calm"],
+  ideals: ["Justice", "Freedom", "Power", "Knowledge"],
+  bonds: ["Family", "Friends", "Mentor", "Community"],
+  flaws: ["Greedy", "Arrogant", "Lazy", "Stubborn"],
+};
+
+export default features;
